076041b376b8f44185b20bb1ef51074a28a39980
BrunoRungue <bruno.rungue21@gmail.com>
2026-08-24 17:11:15 -0300

    Primeiro commit do projeto

---
+++ src/test/java/git/github/com/BrunoRungue/cursoudemy/git/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package git.github.com.BrunoRungue.cursoudemy.git.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
